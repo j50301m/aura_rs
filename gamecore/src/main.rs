6ec8187ce9abc5a2277fd8e38d766b1543ceba70
jasonYu <j50301m@gamil.com>
2025-09-04 08:13:43 +0800

    Initialize Rust workspace with common, gameapi, and gamecore packages
    - Created Cargo.toml for workspace and defined dependencies.
    - Added common package with dependencies on cfgloader and cfgloader_derive.
    - Implemented basic structure for gameapi and gamecore packages with actix-web.
    - Added .env file for gameapi with database configuration.
    - Implemented a simple HTTP server in both gameapi and gamecore with a test for the root endpoint.

---
+++ gamecore/src/main.rs
@@ -0,0 +1,41 @@
+use actix_web::{App, HttpRequest, HttpServer, web};
+
+
+async fn index(req: HttpRequest) -> &'static str {
+    println!("Received request: {}", req.path());
+    "Hello, World!"
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new( || {
+        App::new()
+        .service(web::resource("/").to(index))
+    })
+    .bind("127.0.0.1:5566")?
+    .run()
+    .await
+}
+
+#[cfg(test)]
+mod tests {
+    use actix_web::{body::to_bytes, dev::Service, http, test, Error};
+
+    use super::*;
+
+    #[actix_web::test]
+    async fn test_index() -> Result<(), Error> {
+        let app = App::new().route("/", web::get().to(index));
+        let app = test::init_service(app).await;
+
+        let req = test::TestRequest::get().uri("/").to_request();
+        let resp = app.call(req).await?;
+
+        assert_eq!(resp.status(), http::StatusCode::OK);
+
+        let response_body = resp.into_body();
+        assert_eq!(to_bytes(response_body).await?, r##"Hello, World!"##);
+
+        Ok(())
+    }
+}
